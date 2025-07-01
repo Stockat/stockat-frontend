079e512430dceb0a7a9d48ed4b15596d60ecf488
Esraa Abas <esraa.abbas19@gmail.com>
2025-07-01 03:46:21 +0300

    Feat(Auction): live auctions and bid components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -30,4 +30,13 @@ export const routes: Routes = [
         path: 'admin',
         component: AdminLayoutComponent
     },
+    //Auction
+    {
+        path: 'auctions',
+        loadComponent: () => import('./features/Auction/auctions-list/auctions-list.component').then(m => m.AuctionsListComponent)
+    },
+    {
+        path: 'auction/:id',
+        loadComponent: () => import('./features/Auction/auction-details/auction-details.component').then(m => m.AuctionDetailsComponent)
+    }
 ];
