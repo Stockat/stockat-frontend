ee9fc19c2046c16bb04e9b45fa2d1361f1e23cee
Ali <Ali@DESKTOP-HT66DTE>
2025-07-07 22:25:33 +0300

    feat: Order Dashboard

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -6,6 +6,10 @@ export const adminRoutes: Routes = [
         loadComponent: () => import('./orders/orders.component').then(m => m.AdminOrdersComponent)
     },
     {
+        path: 'orders/analysis',
+        loadComponent: () => import('./orders/order-analysis/order-analysis.component').then(m => m.OrderAnalysisComponent)
+    },
+    {
         path: 'products',
         loadComponent: () => import('./products/manageproducts/manageproducts.component').then(m => m.ManageproductsComponent)
     }
