8530db5a569f2a4efe0ca447ff0797c705e4cb2f
Ali <Ali@DESKTOP-HT66DTE>
2025-07-16 00:44:55 +0300

    fix: Audit Url

--- src/app/core/services/audit.service.ts
+++ src/app/core/services/audit.service.ts
@@ -4,18 +4,18 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AuditDto } from '../models/audit-models/audit.dto';
 import { GenericResponseDto } from '../models/user-models/generic-response.dto';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuditService {
-  private apiUrl = 'http://localhost:5250/api/Audit';
-
+  private baseUrl = `${environment.apiUrl}/api/Audit`;
   constructor(private http: HttpClient) {}
 
   getAuditData(): Observable<AuditDto[]> {
     return this.http
-      .get<GenericResponseDto<AuditDto[]>>(this.apiUrl)
+      .get<GenericResponseDto<AuditDto[]>>(this.baseUrl)
       .pipe(map((response) => response.data));
   }
 }
